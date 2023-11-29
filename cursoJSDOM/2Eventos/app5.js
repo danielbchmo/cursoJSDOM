7c0aaecc6382b79f962508097b9c9877bbe373ba
danielbchmo <danielbchmo123@gmail.com>
2023-11-29 10:54:22 -0600

    Create app5.js

---
+++ cursoJSDOM/2Eventos/app5.js
@@ -0,0 +1,16 @@
+/** 
+ * Matches() y Closest()
+ * Matches se utiliza en cualquier nodo del tipo elemento, recibe un selector CSS como
+ * parametro, si coincide recive true.
+*/
+const wrapper = document.querySelector('[data-id="wrapper"]');
+
+const input = document.querySelector('[type="text"]');
+const changeUser = document.querySelector('[data-id="name"]');
+
+const section = document.querySelector(".section");
+
+const buttonShow = document.querySelector('[data-id="button-show"]');
+const buttonHide = document.querySelector('[data-id="button-hide"]');
+
+ 
