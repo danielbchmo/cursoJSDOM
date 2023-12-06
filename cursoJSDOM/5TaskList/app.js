bef0966106f177386cf7adee253eca025bbb6704
danielbchmo <danielbchmo123@gmail.com>
2023-12-06 09:43:31 -0600

    Finishing 4 task and list

--- cursoJSDOM/5TaskList/app.js
+++ cursoJSDOM/5TaskList/app.js
@@ -23,10 +23,18 @@ message();
 
 function message () {
     const h3 = document.createElement('h3');
+    h3.classList.add('task-message')
 
     h3.textContent = listContainer.firstElementChild ? 'Tareas por hacer' : 'No hay tareas aun';
 
+    const prevMessage = document.querySelector('.task-message');
+    
     //listContainer.parentElement.insertBefore(h3, listContainer);
+    if(prevMessage){
+        //Remplazará un valor h3 en caso de que tenga datos
+        prevMessage.replaceWith(h3);
+        return;
+    }
     listContainer.before(h3);
 }
 function handleSubmit(e) {
@@ -35,6 +43,7 @@ function handleSubmit(e) {
     createTask(inputValue);
 
     this.reset(); //resetear los valores
+    message();
 }
 
 function createTask (value) {
@@ -45,4 +54,48 @@ function createTask (value) {
     //listContainer.appendChild(newTask);
     //Permite agregar varios nodos a la vez
     listContainer.append(newTask);
+
+    addEvents(newTask);
 }
+
+function addEvents(element){
+    //                        Evento doble click
+    element.addEventListener('dblclick', function(){
+
+        //Eliminar elemento, dos formas:
+        // element.parentElement.removeChild(element);
+        element.remove(element);
+        message();
+    } );
+}
+
+/**
+ * insertAdjacentHTML
+ *  Este permite insertar codigo HTML directo en el script.
+ *  Recibe dos parametros:
+ *      donde va a empezar
+ *      codigo HTML
+ * (CON ESTE PUEDE HABER INSERCCIÓN DE CODIGO)
+ * 
+ * insertAdjacentElement
+ *  Este nos permite agregar un elemento dentro de un nodo especifico
+ * 
+ * cloneNode
+ *  Clona un nodo:
+ *      true: va a copiar cada elemento del nodo, incluyendo descendientes
+ *      false: solo copia el nodo, sin descendientes.addEventListener('type', listener, options)
+ */
+
+const wrapper = document.querySelector('.wrapper');
+wrapper.insertAdjacentHTML('beforebegin',`
+    <h1>HTML creado con JS</h1>
+    <button>Dame click</button>
+`);
+
+const h2 = document.querySelector('h2');
+h2.textContent = 'Hola Amigos';
+wrapper.insertAdjacentElement('beforebegin', h2);
+
+const copyWrapper = wrapper.cloneNode(true);
+
+wrapper.before(copyWrapper);
